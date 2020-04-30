6325dd427b9bb1bc0fb66112fdaf5a5455fc002c
Sergey <simakovserg@gmail.com>
2020-04-30 12:06:45 +0500

    Добавлен каталог quotebook. Скрипт генерирует рандомные цитаты моего командира РХБЗ

---
+++ quotebook/quotes.py
@@ -0,0 +1,20 @@
+#! python
+# -*- coding: utf-8 -*-
+
+import random
+import sys
+
+# Открываем файл с базой цитат
+try:
+    with open('quotes.txt', encoding='utf-8', newline='') as fileQuotes:
+        quotesLst = []
+        for line in fileQuotes:
+            quotesLst.append(line)          
+except IOError as e:                                            # обработки ошибки чтения
+    print(f'Ошибка чтения файла!\n{str(e)}')                    # вывод ошибки
+    sys.exit(0) 
+
+# Выбираем случайный индекс из списка
+quoteIndex = random.randint(0,len(quotesLst))
+# Печатаем в консоль случайную цитату
+print(quotesLst[quoteIndex])
