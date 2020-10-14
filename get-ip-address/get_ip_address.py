6190715a149f8ec42a7ea4bd4351347f1841ecf7
Sergey <simakovserg@gmail.com>
2020-10-14 08:56:47 +0500

    Добавлен скрипт пример получения IP адреса

---
+++ get-ip-address/get_ip_address.py
@@ -0,0 +1,10 @@
+#!C:\Python38\python.exe
+# -*- coding: utf-8 -*-
+
+# Как получить IP адрес любого сайта используя Python
+
+import socket
+
+target = 'vk.com'
+
+print(f'IP адрес сайта {target}: {socket.gethostbyname(target)}')
