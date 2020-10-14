44abe287b88de895a4a309924007f3899f22fafd
Sergey <simakovserg@gmail.com>
2020-10-14 10:52:03 +0500

    Добавлен скрипт создающий QR код

---
+++ qr-code/qr_code.py
@@ -0,0 +1,13 @@
+#!C:\Python38\python.exe
+# -*- coding: utf-8 -*-
+
+# Свой QR-код на Python
+
+import pyqrcode
+# pip install pyqrcode pypng
+
+QRString = 'https://t.me/python2day'
+
+url = pyqrcode.create(QRString)
+
+url.png('my_qr.png', scale=8)
