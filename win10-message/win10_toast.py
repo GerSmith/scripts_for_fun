87fb1134c2cc203f1866a1193942f5d477c54edd
Sergey <simakovserg@gmail.com>
2020-10-15 15:54:25 +0500

    Добавлен скрипт делающий всплывающее уведомление для Windows10

---
+++ win10-message/win10_toast.py
@@ -0,0 +1,12 @@
+#!C:\Python38\python.exe
+# -*- coding: utf-8 -*-
+
+# Всплывающее уведомление Windows в 3 строчки на Python
+# pip install win10toast
+
+import win10toast
+
+toaster = win10toast.ToastNotifier()
+toaster.show_toast('Привет от Python!\n'
+                   'И библиотеки win10toast!',
+                   duration=10)
