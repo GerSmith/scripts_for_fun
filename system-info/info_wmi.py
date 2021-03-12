1bdd6409822fb5da9386c00a7bc9c7b8c4e604ce
Sergey <simakovserg@gmail.com>
2021-03-12 10:22:00 +0500

    Добавлен system info

---
+++ system-info/info_wmi.py
@@ -0,0 +1,18 @@
+#! python
+# -*- coding: utf-8 -*-
+
+"""Получение информации о системе."""
+
+
+# https://github.com/tjguk/wmi
+# pip install wmi
+import wmi
+
+wmi_obj = wmi.WMI()
+my_system = wmi_obj.Win32_ComputerSystem()[0]
+
+print(f'Manufacturer: {my_system.Manufacturer}')
+print(f'Model: {my_system.Model}')
+print(f'Name: {my_system.Name}')
+print(f'NumberOfProcessors: {my_system.NumberOfProcessors}')
+print(f'SystemType: {my_system.SystemType}')
